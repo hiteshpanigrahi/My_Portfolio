5428bebe6ced3e3f94b10677dfa6e24e65ada970
hiteshpanigrahi <hitesh.edu9@gmail.com>
2025-01-11 19:13:46 +0530

    Huge Changes Made

--- script.js
+++ script.js
@@ -25,3 +25,34 @@ themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem('darkmode')
   darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxAY78Uj8m33n19b3ylqEl3fiAM9ltshYWu9NNpC8DV_Bw_R-ejbIlzZO2SYyew6ba5/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message sent!"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
